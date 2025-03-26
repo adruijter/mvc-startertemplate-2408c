7bd9d47c7981fb3dcf9f9e2830b823bbe102c70c
adruijter <adruijter@gmail.com>
2025-03-26 08:38:49 +0100

    Vulkaan toets

--- app/db/createscript.sql
+++ app/db/createscript.sql
@@ -653,3 +653,60 @@ VALUES
  ,('Taylor Swift', 570,'Verenigde Staten', '+13421 231356', 34, 1, NULL, SYSDATE(6), SYSDATE(6))
  ,('Beyoncé', 420,'Verenigde Staten', '+18723 213481', 41, 1, NULL, SYSDATE(6), SYSDATE(6))
  ,('Jennifer Lopez', 400,'Verenigde Staten', '+16254 751243', 54, 1, NULL, SYSDATE(6), SYSDATE(6));
+
+
+
+ 
+-- Step: 14
+-- *****************************************************************************************************
+-- Doel : Maak een nieuwe tabel aan met de naam Vulkaan
+-- *****************************************************************************************************
+-- Versie       Datum           Auteur              Omschrijving
+-- ******       *****           ******              ************
+-- 01           26-03-2025      Arjan de Ruijter    Tabel Vulkaan
+-- *****************************************************************************************************
+-- Onderstaande velden zelf toevoegen aan de tabel Vulkaan
+-- *****************************************************************************************************
+
+CREATE TABLE Vulkaan
+(
+     Id                       INT            UNSIGNED    NOT NULL        AUTO_INCREMENT
+    ,Naam                     VARCHAR(250)               NOT NULL
+    ,Hoogte                   INT            UNSIGNED    NOT NULL
+    ,Land                     VARCHAR(250)               NOT NULL
+    ,JaarLaatsteUitbarsting   VARCHAR(250)               NOT NULL
+    ,AantalSlachtoffers       INT            UNSIGNED    NOT NULL
+    ,IsActief                 BIT                        NOT NULL        DEFAULT 1
+    ,Opmerking                VARCHAR(255)                   NULL        DEFAULT NULL
+    ,DatumAangemaakt          DATETIME(6)                NOT NULL
+    ,DatumGewijzigd           DATETIME(6)                NOT NULL
+    ,CONSTRAINT               PK_Zangeres_Id             PRIMARY KEY     CLUSTERED(Id)
+) ENGINE=InnoDB;
+
+-- Step: 15
+-- *****************************************************************
+-- Doel : Vul de tabel Vulkaan met gegevens
+-- *****************************************************************
+-- Versie       Datum           Auteur              Omschrijving
+-- ******       *****           ******              ************
+-- 01           26-3-2025      Arjan de Ruijter     Vulling Vulkaan
+-- *****************************************************************
+
+INSERT INTO Vulkaan
+(
+      Naam
+     ,Hoogte
+     ,Land
+     ,JaarLaatsteUitbarsting
+     ,AantalSlachtoffers
+     ,IsActief
+     ,Opmerking
+     ,DatumAangemaakt
+     ,DatumGewijzigd
+)
+VALUES
+  ('Eyjafjallajökul', 1666,'IJsland', '2010', 1, 1, NULL, SYSDATE(6), SYSDATE(6))
+ ,('Vesuvius', 1281,'Italië', '1944', 26, 1, NULL, SYSDATE(6), SYSDATE(6))
+ ,('Sakurajima', 1117,'Japan', '2012', 2, 1, NULL, SYSDATE(6), SYSDATE(6))
+ ,('Merapi', 2910,'Indonesië', '2020', 5, 1, NULL, SYSDATE(6), SYSDATE(6))
+ ,('Nyiragongo', 3470,'Democratische Republiek Congo', '2021', 15, 1, NULL, SYSDATE(6), SYSDATE(6));
